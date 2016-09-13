552aa76282db7df720d97b55f99aedab06a4fff9
Aaron Diaz R <aadroa@gmail.com>
2016-09-13 16:32:09 -0500

    Se quito la palabra only del framework
    Esto permite convertir numeros a letras, pero corrigiendo algunos detalles en la traduccion

--- frappe/utils/data.py
+++ frappe/utils/data.py
@@ -438,7 +438,7 @@ def money_in_words(number, main_currency = None, fraction_currency=None):
 	if cint(fraction):
 		out = out + ' ' + _('and') + ' ' + in_words(fraction, in_million).title() + ' ' + fraction_currency
 
-	return out + ' ' + _('only.')
+	return out
 
 #
 # convert number to words
